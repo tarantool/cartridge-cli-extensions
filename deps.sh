031ab895e310a96b435e3b7d2c436b11caf2462c
Igor Zolotarev <63460867+yngvar-antonsson@users.noreply.github.com>
2022-12-14 23:25:05 +0400

    Add new Tarantool versions in tests (#13)

--- deps.sh
+++ deps.sh
@@ -4,5 +4,5 @@
 set -e
 
 # Test dependencies:
-tarantoolctl rocks install luatest 0.5.0
-tarantoolctl rocks install luacheck 0.25.0
+tarantoolctl rocks install luatest 0.5.7
+tarantoolctl rocks install luacheck 0.26.0
